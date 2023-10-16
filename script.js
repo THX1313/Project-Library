553da93835e8561ddf1a4f8a9a2c5e5fbfe6f1db
William <williamhess@gmail.com>
2023-10-16 11:13:27 -0700

    Add display to Dom

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-    // the constructor...
+const bookCards = document.querySelector('.bookCards');
+    
+
+// The constructor...
 function Book(author, title, numberOfPages, beenRead) {
     this.author = author;
     this.title = title;
@@ -10,7 +13,7 @@ function Book(author, title, numberOfPages, beenRead) {
 const myLibrary = [];
 
 
-// Prepopulate the library with some books
+// Populate the library with some books
 myLibrary.push(new Book("Andy Wier", "The Martian", 387, true));
 myLibrary.push(new Book("Max Brooks", "World War Z", 487, false));
 myLibrary.push(new Book("George Orwell", "1984", 328, true));
@@ -24,23 +27,47 @@ function addBookToLibrary() {
   const bookAuthor = prompt("Enter the book's author: ");
   const bookNumberOfPages = parseInt(prompt("Enter the book's number of pages: "));
   const bookBeenRead = prompt("Have you read the book? (yes or no)").toLowerCase();
-
   const beenRead = bookBeenRead === "yes";
   myLibrary.push(new Book(bookAuthor, bookTitle, bookNumberOfPages, beenRead));
 }
 
-// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
+// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”.
 //
 // Function to display books in the library
 function displayBooks() {
+  // Remove all of the element's child nodes to initialize the display of books to null
+  while (bookCards.firstChild) {
+    bookCards.removeChild(bookCards.firstChild);
+  }
+  // Generate a book card, and add it to the web page, for each book in the library
+  generateBookCards();
+}
+
+function generateBookCards() {
+  // Loop through the array of books
   myLibrary.forEach((book, index) => {
-    console.log(`Book ${index + 1}:
-    Title: ${book.title}
-    Author: ${book.author}
-    Number of Pages: ${book.numberOfPages}
-    Been Read: ${book.beenRead ? "Yes" : "No"}`);
+    // create a new div element
+    const bookCard = document.createElement("div");
+
+    // and give it some content
+    const newContent = document.createTextNode(`Book: ${index + 1}
+      Title: ${book.title}
+      Author: ${book.author}
+      Number of Pages: ${book.numberOfPages}
+      Been Read: ${book.beenRead ? "Yes" : "No"}`);
+
+    // add the text node to the newly created div
+    bookCard.appendChild(newContent);
+
+    // add the newly created element and its content into the DOM
+    // const currentDiv = document.getElementById("div1");
+    // document.body.insertBefore(newDiv, currentDiv);
+
+    // Add book card to document
+    bookCards.appendChild(bookCard);
   });
 }
+    
 // Add a button on each book’s display to remove the book from the library.
 // You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
@@ -54,26 +81,3 @@ addBookToLibrary();
 
 // Display the books
 displayBooks();
-
-
-
-
-
-
-
-// myLibrary.push(new Book('Andy Wier', 'The Martian', 387, 'true'));
-// myLibrary.push(new Book('Max Brooks', 'World War Z', 487, 'false'));
-// myLibrary.push(new Book('1984', 'George Orwell', 328, 'true'));
-
-// getBookData();
-
-
-
-// myLibrary.forEach((book) => {
-//   console.log(book);
-// });
-
-// function addBookToLibrary() {
-//     // do stuff here
-//   }
-
